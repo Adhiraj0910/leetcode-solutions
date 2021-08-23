a8fab2b3856d085c99152d6f9fe322d0e10f2593
Mac <adhirajagarwal.aa7@gmail.com>
2021-08-23 14:13:40 -0400

    68/68 testcases passed

---
+++ 198.house-robber.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=198 lang=python3
+#
+# [198] House Robber
+#
+
+# @lc code=start
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        dp=[0]*len(nums)
+        dp[0]=nums[0]
+        dp[1]=max(nums[1],nums[0])
+        for i in range(2,len(nums)):
+            dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+        return dp[-1]
+
+
+        
+# @lc code=end
+
